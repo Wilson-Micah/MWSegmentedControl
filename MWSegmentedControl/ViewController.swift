838cdea9c3582b3635725eae82b1327a3b08726f
Micah T Wilson <micahw@sisna.com>
2015-09-08 12:06:28 -0600

    Initial Commit

---
+++ MWSegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MWSegmentedControl
+//
+//  Created by Micah Wilson on 9/8/15.
+//  Copyright © 2015 Micah Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
